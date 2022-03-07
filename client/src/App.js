0638952acd9f177e4eec0dfdd2e5240f8b6570d9
maryammaminn <maryammaminn@gmail.com>
2022-03-07 20:03:22 +0200

    ResponsiveDrawer

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -11,11 +9,11 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
-
 import MovieCard from "./components/MovieCard"
 import MoviePage from "./components/MoviePage"
 import SearchAppBar from "./components/SearchAppBar"
 import AllMovies from "./components/AllMovies"
+import ResponsiveDrawer from './components/ResponsiveDrawer';
 
 function App() {
   return (
@@ -24,12 +22,18 @@ function App() {
         <SearchAppBar />
         <Routes>
 
-          <Route path="/" element={<AllMovies searchFlag={false} />} />
+          <Route path="/" element={<AllMovies searchFlag={false} filterFlag={false} />} />
           <Route path="/:movieID" element={<MoviePage />} />
-          <Route path="/search/:q" element={<AllMovies searchFlag={true} />} />
+          <Route path="/search/:q" element={<AllMovies searchFlag={true} filterFlag={false}/>} />
+          <Route path="/filter/:category" element={<AllMovies filterFlag={true} searchFlag={false} />} />
 
         </Routes>
+        
       </BrowserRouter>
+
+      {/* <ResponsiveDrawer/> */}
+
+
     </div>
   );
 }
