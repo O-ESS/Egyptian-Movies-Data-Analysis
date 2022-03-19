51dfc890f8c260f3b5eeb18db4be15b114a6826a
omar essam <third3pjc@gmail.com>
2022-03-19 16:08:39 +0200

    finished with login and register

--- client/src/components/MoviePage.js
+++ client/src/components/MoviePage.js
@@ -9,6 +9,9 @@ import Typography from '@mui/material/Typography';
 import React, { useState, useEffect } from "react";
 import { useParams } from 'react-router-dom'
 import axios from "axios";
+import { textAlign } from '@mui/system';
+import { Rating } from 'react-simple-star-rating'
+import { Alert } from '@mui/material';
 
 
 
@@ -16,6 +19,33 @@ export default function MoviePage() {
 
     const [movieData, setMovieData] = useState();
     const { movieID } = useParams()
+
+
+    function StarRating() {
+        const [rating, setRating] = useState(0) // initial rating value
+      
+        // Catch Rating value
+        const handleRating = (rate) => {
+          console.log(rate)
+          setRating(rate)
+          axios.post('http://localhost:8080/auth/rate' , movieID, rating, localStorage.getItem('user token') )
+          .then((result) => {
+            console.log(result.data.message);
+            setMovieData(result.data.updatedMovie)
+          }).catch(err => {
+            console.log(err);
+            alert("you have to be logged in to rate movies");
+        });
+          // other logic
+        }
+      
+        return (
+          <div className='App'>
+            <Rating onClick={handleRating} ratingValue={rating} /* Available Props */ />
+          </div>
+        )
+      }
+
 
     useEffect(() => {
         axios.get(`http://localhost:8080/${movieID}`)
@@ -29,41 +59,76 @@ export default function MoviePage() {
 
     return (
         <>
+        
             {movieData &&
-                <Card sx={{   width: 1000,
-                    height: 1000, paddingX:100 }}>
+                <Card sx={{   width: 2030,
+                    height: 1000}}>
                     <CardMedia
                         component="img"
                         alt="film"
-                        height="180"
+                        height="400"
+                        width= "2030"
                         image="https://source.unsplash.com/Lq6rcifGjOU"
                     // image="../../public/static/film.png"
                     />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
+                    <CardContent >
+                        <Typography gutterBottom variant="h2" component="div" style={{
+        display: "flex",
+        justifyContent: "flex-end",
+        textAlign: "right"
+      }}>
                             {movieData.Name}
                         </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            {movieData.Synopsis}
+                        <Typography variant="h4" color="text.secondary" style={{
+        display: "flex",
+        justifyContent: "flex-end",
+        textAlign: "right"
+      }}>
+                            الملخص: {movieData.Synopsis}
                         </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            {movieData.rate}
+                        <Typography variant="h4" color="text.secondary" style={{
+        display: "flex",
+        justifyContent: "flex-end",
+        textAlign: "right"
+      }}>
+                           التقييم:  {Number((movieData.rate).toFixed(2))} 
                         </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            {movieData.Category}
+                        <Typography variant="h4" color="text.secondary" style={{
+        display: "flex",
+        justifyContent: "flex-end",
+        textAlign: "right"
+      }}>
+                          التصنييف: {movieData.Category}
                         </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            {movieData.Actors}
+                        <Typography variant="h4" color="text.secondary" style={{
+        display: "flex",
+        justifyContent: "flex-end",
+        textAlign: "right"
+      }}>
+                          الممثلين:   {movieData.Actors}
                         </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            {movieData.Duration}
+                        <Typography variant='h4' color="text.secondary" style={{
+        display: "flex",
+        justifyContent: "flex-end",
+        textAlign: "right"
+      }}>
+                          المده: {movieData.Duration}
                         </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            {movieData.Date}
+                        <Typography variant="h4" color="text.secondary" style={{
+        display: "flex",
+        justifyContent: "flex-end",
+        textAlign: "right"
+      }}>
+                           تاريخ الأصدار:  {movieData.Date}
                         </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            {movieData.Leading_actor}
+                        <Typography variant="h4" color="text.secondary" style={{
+        display: "flex",
+        justifyContent: "flex-end",
+        textAlign: "right",
+      }}>
+                           البطوله:  {movieData.Leading_actor}
                         </Typography>
+                        <StarRating/> <Button > Delete old rating </Button>
                     </CardContent>
                     {/* <CardActions>
         <Button size="small">Share</Button>
--- server/routes/user.js
+++ server/routes/user.js
@@ -21,12 +21,12 @@ const auth = (req, res, next) => {
 
 router.post('/login', async (req, res) => {
     try {
-        const { email , password } = req.body
+        const { username , password } = req.body
         // const { error } = loginValidation(req.body);
         // if (error) return res.status(400).json(error.details[0].message);
 
-        const foundUser = await User.findOne({ email: email });
-        if (!foundUser) return res.status(400).send("Email not found");
+        const foundUser = await User.findOne({ name: username });
+        if (!foundUser) return res.status(400).send("Username not found");
         
         if (foundUser.password !== password) return res.status(400).send("Incorrect password");
         // const isMatched = await bcrypt.compare(req.body.password, foundUser.password);
@@ -69,7 +69,7 @@ router.post('/rate',auth, async (req, res) => {
             if(movie.movieID == movieID)
                 res.json("you rated this movie before , to re-rate it delete the old rate then re-rate again");
         });
-
+       console.log(newRate)
         const updatedUser = await User.findByIdAndUpdate(userID, { $addToSet: { rates: movieObject } },
             { new: true, runValidators: true, useFindAndModify: true })
 
@@ -84,6 +84,43 @@ router.post('/rate',auth, async (req, res) => {
         console.log("🚀 ~ file: user.js ~ line 79 ~ router.post ~ err", err)
     }
 });
+
+
+
+router.post('/deleterate',auth, async (req, res) => {
+    try {
+        const userID = req.user.id
+        const {movieID , rate} = req.body 
+        const foundUser = await User.findById(userID)
+        const foundMovie = await Movie.findById(movieID)
+        const sum = Number(foundMovie.users.length)
+        const oldRate = Number(foundMovie.rate)
+        const newRate = (oldRate*sum + Number(rate)) / (sum+1)
+
+        const movieObject = {movieID , rate} //will ba added into rates array in user object
+        const userObject = {userID , rate} //will be added into users array in movie object then avg rate will be calculated
+
+        var __FOUND = foundUser.rates.find(function(movie, index) {
+            if(movie.movieID == movieID){
+                res.json("you rated this movie before , to re-rate it delete the old rate then re-rate again");
+            }
+        });
+
+        const updatedUser = await User.findByIdAndDelete(userID, { $addToSet: { rates: movieObject } },
+            { new: true, runValidators: true, useFindAndModify: true })
+
+        const updatedMovie = await Movie.findByIdAndUpdate(movieID, { $addToSet: { users: userObject } , rate : newRate },
+            { new: true, runValidators: true, useFindAndModify: true })
+
+
+        res.json({updatedUser ,updatedMovie });
+
+    } catch (err) {
+        res.json(err);
+        console.log("🚀 ~ file: user.js ~ line 79 ~ router.post ~ err", err)
+    }
+});
+
 
 
 
